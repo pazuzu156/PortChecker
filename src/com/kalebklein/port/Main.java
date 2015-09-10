5821d27f15ddad793a5b47cd1eb6c09f72b446ad
Kaleb Klein <klein.jae@gmail.com>
2015-09-09 21:47:31 -0400

    Updated Launch4j config

--- src/com/kalebklein/port/Main.java
+++ src/com/kalebklein/port/Main.java
@@ -7,7 +7,7 @@ import com.kalebklein.updater.UpdateCheck;
 
 public class Main
 {
-	public static final int VERSION_CODE = 7;
+	public static final int VERSION_CODE = 8;
 	public static final String VERSION = "1.2";
 
 	private String hostname;
